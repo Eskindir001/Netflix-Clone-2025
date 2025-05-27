d6a53f77aa048350c00b8f3fa49922223679f32c
Eska12345678 <94687142+Eska12345678@users.noreply.github.com>
2025-05-27 03:19:51 +0300

    change

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Fotter/Footer'
+import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/RowList/RowList'
+
+const Home = () => {
+  return (
+      <>
+      <Header />
+
+
+      <Banner />
+    
+      <RowList/>
+          <Footer/>
+
+    </>
+  )
+}
+
+export default Home
---
+++ netflix-clone/src/utils/requests.js
@@ -0,0 +1,7 @@
+
+const requests = {
+    fetchTrending: '/trending/all/week?api_key=YOUR_API_KEY',
+    fetchNetflixOriginals: '/discover/tv?api_key=YOUR_API_KEY&with_networks=213',
+  };
+  
+  export default requests;
